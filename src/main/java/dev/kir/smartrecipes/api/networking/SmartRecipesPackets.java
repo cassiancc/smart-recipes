527906983419c1b97e31da71985163a65b5d8853
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-10-08 20:46:55 -0700

    It launches!

--- src/main/java/dev/kir/smartrecipes/api/networking/SmartRecipesPackets.java
+++ src/main/java/dev/kir/smartrecipes/api/networking/SmartRecipesPackets.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 public class SmartRecipesPackets {
     @Environment(EnvType.CLIENT)
     public static void initClient() {
-        PayloadTypeRegistry.playC2S().register(SynchronizeReloadedRecipesPacket.ID, SynchronizeReloadedRecipesPacket.CODEC);
+        PayloadTypeRegistry.playS2C().register(SynchronizeReloadedRecipesPacket.ID, SynchronizeReloadedRecipesPacket.CODEC);
 
         ClientPlayNetworking.registerGlobalReceiver(SynchronizeReloadedRecipesPacket.ID, (payload, context) -> payload.execute(context.client()));
 
